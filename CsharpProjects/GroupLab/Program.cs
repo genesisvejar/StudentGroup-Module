1d4a86c9a9e17ea032f4e47d355a6d1f7d719610
Niale724 <elainwongleo@gmail.com>
2025-09-05 05:06:42 +0800

    added funstions: addMember, retrieveMember, displayGroup

--- CsharpProjects/GroupLab/Program.cs
+++ CsharpProjects/GroupLab/Program.cs
@@ -20,4 +20,57 @@
 
     }
 
+     //allow adding a student’s name at a specific position in the group
+    //if the position is invalid, the system will return an error message
+    public void addMember(string stuName, int position)
+    {
+        if (position < 0 || position >= numMembers)
+        {
+            Console.WriteLine("Error: Invalid position.");
+        }
+        else
+        {
+            for (int count = 0; count < numMembers; count++)
+            {
+                if (count == position)
+                {
+                    studentNames[count] = stuName;
+                    Console.WriteLine($"Success:{stuName} added to position{count}.");
+                }
+            }
+        }
+    }
+
+    //allow retrieving the student’s name when given their position
+    //system will return an error message if position is invalid
+    public string retrieveMember(int index)
+    {
+        if (index < 0 || index >= numMembers)
+        {
+            return "Error: Invalid position.";
+        }
+        else
+        {
+            for (int count = 0; count < numMembers; count++)
+            {
+                if (count == index)
+                {
+                    return studentNames[count];
+                }
+            }
+        }
+
+    }
+
+    //system will display the group name and name of all students in group
+    public void displayGroup()
+    {
+        Console.WriteLine($"Group: {groupName}");
+        for (int count = 0; count < numMembers; count++)
+        {
+            Console.WriteLine(studentNames[count]);
+        }
+
+    }
+
 }
