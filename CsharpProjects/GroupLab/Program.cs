6483fc9b651f017ba9824c4911628ac48a3e11c7
genesisvejar <genesisvejar09@gmail.com>
2025-09-04 14:52:52 -0400

    Create a class and add a constructor

--- CsharpProjects/GroupLab/Program.cs
+++ CsharpProjects/GroupLab/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class StudentGroup
+{
+    //private variables:
+
+    private string groupName;
+    private int numMembers;
+
+    private string [] studentNames;
+
+    //public methods
+
+    public StudentGroup(string groName, int groSize)
+
+    {
+        groupName = groName;
+
+        numMembers = groSize;
+
+        studentNames = new string[groSize];
+
+    }
+
+}
